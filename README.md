b18eeed476af6e28c50af17fa5edcf39fe1adb6c
Rohan Das <rohandasbirbhum@gmail.com>
2020-03-23 12:11:47 +0530

    First Commit 🎉

---
+++ README.md
@@ -0,0 +1,16 @@
+
+
+# Covid-19 Tracker and Notifier In Python [![forthebadge](https://forthebadge.com/images/badges/made-with-python.svg)](https://forthebadge.com)
+### This is a simple python script which tracks the stats of Covid 19 Outbreak in India and Displays a Notification in every 1 Hour.
+---
+### Instructions
+ 1. Make sure you have [**Python** ](https://www.python.org/)(Version 3.7 or higher) installed in your system.
+ 2. Open the folder in CMD and type ``` pip install bs4 ``` to install BeautifulSoup, then again type ```pip install plyer``` to install plyer (for the notification) and lastly type ``` pip install requests``` to complete installing the required models for the script.
+ 4. Now edit the **main.py** file using your favorite code editor(Example: Sublime text editor, VSCode, Vim).
+ 5. Enter Your State Name in Line no 30. (Don't enter more than 5 States). (Example: states = ['Delhi', 'West Bengal', 'Maharashtra',])
+ 7. Now save the file, close the editor and run **main.py**.
+
+#### This Program Will Only Work On Windows 10 Currently.
+ ---
+### Be sure to stargaze the repository and also checkout [My Website](https://rohandas28.github.io/) to contact me.
+#### Thank You.
---
+++ main.py
@@ -0,0 +1,38 @@
+import requests
+from plyer import notification
+from bs4 import BeautifulSoup
+import time
+
+def notifyMe(title, message): #To Show The Notification
+    notification.notify(
+        title= title,
+        message = message,
+        app_icon = "favicon.ico",
+        timeout = 20 #Timeout Settings In Seconds
+    )
+
+def webData(url): #Getting The Data From The Website
+    r = requests.get(url)
+    return r.text
+
+if __name__ == "__main__":
+    while True:
+        html_doc = webData('https://www.mohfw.gov.in/')
+        soup = BeautifulSoup(html_doc, 'html.parser') #Parsing The Data
+        mystr = ""
+
+        for tr in soup.find_all ('tbody')[1].find_all('tr'):
+            mystr += tr.get_text() #Converting the Parsed Data to a String        
+        mystr = mystr[1:]
+        myList = (mystr.split("\n\n")) 
+
+
+        states = ['Delhi','West Bengal', 'Maharashtra',] # Enter Your State Name Here (Dont Enter More Than 5 States)
+        for item in myList[0:23]:
+            dataList = (item.split('\n'))
+            if dataList[1] in states:
+                notify_title= 'Cases of Covid-19 In India'
+                notify_text= f" State: {dataList[1]}\n Indian Cases : {dataList[2]} & Foreign Cases : {dataList[3]}\n Cured : {dataList[4]}\n Deaths : {dataList[5]}"
+                notifyMe(notify_title, notify_text)
+                time.sleep(2)
+        time.sleep(3600) #Loop For 1 Hour (1 hour = 3600 seconds)
